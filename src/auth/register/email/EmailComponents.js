51d911c3b1e2441c0467e852aac1860b7755da82
MarveFigone <marvee.social@gmail.com>
2024-05-31 13:48:23 +0200

    email send full functional

---
+++ src/auth/register/email/EmailComponents.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: #fff;
+    padding: 30px 65px;
+    border-radius: 12px;
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+`;
