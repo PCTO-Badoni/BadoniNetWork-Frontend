f0b34f8763faff84dd17a781633b4bb2de55ef1c
VasseDev <pietrovassena01234@gmail.com>
2024-06-06 11:30:35 +0200

    indexOTP.jsx and login.jsx linked to backend

--- src/OTP/indexOTP.jsx
+++ src/OTP/indexOTP.jsx
@@ -1,30 +1,95 @@
 import * as OTPComponents from "./OTPComponents";
 import React from "react";
 import "../styles.css";
+import { toast, ToastContainer, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+
+const error = (message) => toast.error( message, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Bounce
+});
+
+const responseView = (body) => toast.success(body, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Bounce
+});
+
+
 function OTP() {
+    const [num1, setNum1] = React.useState("");
+    const [num2, setNum2] = React.useState("");
+    const [num3, setNum3] = React.useState("");
+    const [num4, setNum4] = React.useState("");
+    const [num5, setNum5] = React.useState("");
+    const [num6, setNum6] = React.useState("");
+
     let navigate = useNavigate();
 
-    const handleOnClick = () => {
-        navigate('/homepage');
+    async function handleOnClick(event){
+       event.preventDefault();
+
+       const codice = num1 + num2 + num3 + num4 + num5 + num6;
+
+       const data = {
+           codice
+       };
+
+       try {
+           const response = await fetch("http://localhost:8080/register/validate-otp", {
+               method: "POST",
+                headers: {
+                     "Content-Type": "application/json"
+                },
+               body: JSON.stringify(data)
+           });
+
+           if (!response.ok) {
+               const errorData = await response.json();
+               error(errorData.message || "Errore durante la verifica del codice OTP");
+               throw new Error("HTTP error! status: " + response.status);
+           }
+
+           const responseData = await response.json();
+           responseView(responseData.message);
+       } catch (error) {
+           console.error("There was an error!", error);
+       }
     }
 
     return (
+        <>
         <OTPComponents.Container>
             <OTPComponents.form>
                 <OTPComponents.Logo />
                 <OTPComponents.h4>Inserisci il codice ricevuto</OTPComponents.h4>
                 <div>
-                    <OTPComponents.otpInput type="text" placeholder=""/>
-                    <OTPComponents.otpInput type="text" placeholder=""/>
-                    <OTPComponents.otpInput type="text" placeholder=""/>
-                    <OTPComponents.otpInput type="text" placeholder=""/>
-                    <OTPComponents.otpInput type="text" placeholder=""/>
-                    <OTPComponents.otpInput type="text" placeholder=""/>
+                    <OTPComponents.otpInput value={num1} onChange={e => setNum1(e.target.value)} type="text" placeholder=""/>
+                    <OTPComponents.otpInput value={num2} onChange={e => setNum2(e.target.value)} type="text" placeholder=""/>
+                    <OTPComponents.otpInput value={num3} onChange={e => setNum3(e.target.value)} type="text" placeholder=""/>
+                    <OTPComponents.otpInput value={num4} onChange={e => setNum4(e.target.value)} type="text" placeholder=""/>
+                    <OTPComponents.otpInput value={num5} onChange={e => setNum5(e.target.value)} type="text" placeholder=""/>
+                    <OTPComponents.otpInput value={num6} onChange={e => setNum6(e.target.value)} type="text" placeholder=""/>
                 </div>
                 <OTPComponents.verifyButton id="verifyButton" onClick={handleOnClick}>Verifica</OTPComponents.verifyButton>
             </OTPComponents.form>
         </OTPComponents.Container>
+        <ToastContainer newestOnTop={true}/>
+        </>
     );
 }
 
--- src/auth/login/login.jsx
+++ src/auth/login/login.jsx
@@ -2,20 +2,70 @@ import * as LoginComponents from "./LoginComponents";
 import ReactDOM from "react-dom";
 import React from "react";
 import "../../styles.css";
+import { toast, ToastContainer, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom"; // Modifica qui
 
+const error = (message) => toast.error( message, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Bounce
+});
+
 function Login() {
+
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    async function handleLogin(event) {
+        event.preventDefault();
+
+        const data = {
+            email,
+            password
+        };
+
+        try {
+            const response = await fetch("http://localhost:8080/login/authenticate", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                error(errorData.message || "Errore durante il login");
+                throw new Error("HTTP error! status: " + response.status);
+            }
+
+            const responseData = await response.json();
+        } catch (error) {
+            console.error("There was an error!", error);
+        }
+    }
+
     return (
+        <>
         <LoginComponents.Container>
-            <LoginComponents.Form>
+            <LoginComponents.Form onSubmit={handleLogin}>
                 <LoginComponents.Title>Bentornato</LoginComponents.Title>
-                <LoginComponents.Input type="email" placeholder="Email" required/>
-                <LoginComponents.Input type="password" placeholder="Password" required/>
+                <LoginComponents.Input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} required/>
+                <LoginComponents.Input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required/>
                 <LoginComponents.Button>Accedi</LoginComponents.Button>
                 <LoginComponents.clickableText to="/forgotPassword">Password dimenticata?</LoginComponents.clickableText>
                 <LoginComponents.clickableText to="/">Non hai un account? Registrati</LoginComponents.clickableText>
             </LoginComponents.Form>
         </LoginComponents.Container>
+        <ToastContainer newestOnTop={true}/>
+        </>
     );
 }
 export default Login;
